42fbeedc5997d613eb31f3ba00104576945407d5
Teja332 <153491530+Firefox672@users.noreply.github.com>
2025-10-26 01:25:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/tracksheet',
   trailingSlash: true,
-  distDir: 'out',
 };
-
 export default nextConfig;
